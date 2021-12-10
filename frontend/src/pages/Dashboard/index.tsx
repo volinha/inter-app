5612ec4b143f8507b08f3258ce07d1732f8a7306
Vinícius Fernandes <viniciusfernandesmct@gmail.com>
2021-12-10 01:04:45 -0300

    updated: dashboard

--- frontend/src/pages/Dashboard/index.tsx
+++ frontend/src/pages/Dashboard/index.tsx
@@ -1,61 +1,102 @@
-import {DashboardBackground, BodyContainer, InlineContainer, InlineTitle} from './styles';
+import { useEffect, useState } from 'react';
+import { DashboardBackground, BodyContainer, InlineContainer, InlineTitle } from './styles';
 
 import Header from '../../components/Header';
 import Card from '../../components/Card';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
+import useAuth from '../../hooks/useAuth';
+
+import { pay, request } from '../../services/resources/pix';
 
 import Statement from './Statement';
 
 const Dashboard = () => {
+    const { user, getCurrentUser } = useAuth();
 
-    const wallet = 6000.00
+    const wallet = user?.wallet || 0;
+
+    const [key, setKey] = useState('');
+    const [generatedKey, setGeneratedKey] = useState('');
+    const [value, setValue] = useState('');
+
+    const handleNewPayment = async () => {
+        const { data } = await request(Number(value));
+
+        if (data.copyPasteKey) {
+            setGeneratedKey(data.copyPasteKey);
+        }
+    }
+
+    const handlePayPix = async () => {
+        try{
+            const {data} = await pay(key)
+            if(data.msg){
+                alert(data.msg)
+                return;
+            }
+            alert('Não foi possível completar a transação!')
+        }catch(e){
+            console.log(e)
+            alert('Não é possível receber o PIX do mesmo usuário!')
+        }
+    }
+
+    useEffect(() => {
+        getCurrentUser();
+    }, [])
+
+    if (!user) {
+        return null;
+    }
 
     return (
         <DashboardBackground>
             <Header />
             <BodyContainer>
                 <div>
-                   <Card noShadow width="90%">
-                       <InlineTitle>
-                        <h2 className="h2">Saldo Atual</h2>
-                       </InlineTitle>
-                       <InlineContainer>
+                    <Card noShadow width="90%">
+                        <InlineTitle>
+                            <h2 className="h2">Saldo Atual</h2>
+                        </InlineTitle>
+                        <InlineContainer>
                             <h3 className="wallet">
-                                {wallet.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'})}
+                                {wallet.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })}
                             </h3>
                         </InlineContainer>
-                   </Card>
-                   <Card noShadow width="90%">
-                       <InlineTitle>
-                        <h2 className="h2">Receber PIX</h2>
-                       </InlineTitle>
+                    </Card>
+                    <Card noShadow width="90%">
+                        <InlineTitle>
+                            <h2 className="h2">Receber PIX</h2>
+                        </InlineTitle>
                         <InlineContainer>
-                            <Input style={{flex:1}}/>
-                            <Button>Gerar código</Button>
+                            <Input style={{ flex: 1 }} value={value} onChange={e => setValue(e.target.value)} />
+                            <Button onClick={handleNewPayment}>Gerar código</Button>
                         </InlineContainer>
-                        
-                            <p className="primary-color">Pix copia e cola:</p>
-                            <p className="primary-color">gfd2135fgd789sdgfs12564gfd132</p>
-                        
-                   </Card>
-                   <Card noShadow width="90%">
+                        {generatedKey && (
+                            <>
+                                <p className="primary-color">Pix Copia e Cola:</p>
+                                <p className="primary-color">{generatedKey}</p>
+                            </>
+                        )}
+                    </Card>
+                    <Card noShadow width="90%">
                         <InlineTitle>
                             <h2 className="h2">Pagar PIX</h2>
                         </InlineTitle>
                         <InlineContainer>
-                        <Input/>
-                        <Button>Pagar PIX</Button>
+                            <Input style={{ flex: 1 }} value={key} onChange={e => setKey(e.target.value)} placeholder='Insira a chave' />
+                            <Button onClick={handlePayPix}>Pagar PIX</Button>
                         </InlineContainer>
-                   </Card>
+                    </Card>
                 </div>
                 <div>
                     <Card noShadow width="90%">
-                      <InlineTitle>
-                      <h2 className="h2">Extrato da conta</h2>
-                      </InlineTitle>
-                      <Statement />
-                   </Card>
+                        <InlineTitle>
+                            <h2 className="h2">Extrato da conta</h2>
+                        </InlineTitle>
+                        <Statement />
+                    </Card>
                 </div>
             </BodyContainer>
         </DashboardBackground>
